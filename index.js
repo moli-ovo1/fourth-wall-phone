2ac46a781ba2f59c664eaf05dbe5a509c819ac73
moli-ovo1 <285720382@qq.com>
2026-09-21 22:36:55 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const EMERGENCY_HANDLE_ID = 'moli-phone-handle';
-const MOLI_BUILD_VERSION = '0.6.26';
+const MOLI_BUILD_VERSION = '0.6.27';
 
 function ensureBootstrapLauncher() {
   const existing = document.getElementById(EMERGENCY_HANDLE_ID);
