d8c3810cfd59c798608840e37bb59b663e21edd4
moli-ovo1 <285720382@qq.com>
2026-09-23 00:56:01 +0800

    Add files via upload

--- companion/app/build.gradle.kts
+++ companion/app/build.gradle.kts
@@ -8,7 +8,12 @@ android {
         applicationId = "org.moli.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
     }
+}
+
+
+dependencies {
+    implementation("androidx.work:work-runtime:2.10.1")
 }
