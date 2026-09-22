8735280ba250120f699669ea4610548d14cc34a9
moli-ovo1 <285720382@qq.com>
2026-09-23 00:17:20 +0800

    Add files via upload

--- companion/app/src/main/java/org/moli/companion/transport/CompanionTransport.java
+++ companion/app/src/main/java/org/moli/companion/transport/CompanionTransport.java
@@ -1,1 +1,14 @@
+package org.moli.companion.transport;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/** Transport abstraction only. Phase 1B intentionally does not choose localhost/file/bridge yet. */
+public interface CompanionTransport {
+    JSONObject readLatestWakeRequest(String scopeKey) throws Exception;
+    void appendPendingWakeResult(JSONObject wakeResult) throws Exception;
+    JSONArray readPendingWakeResults(String scopeKey) throws Exception;
+    void acknowledgeWakeResults(String scopeKey, JSONArray wakeIds) throws Exception;
+    JSONObject readLease(String scopeKey) throws Exception;
+    JSONObject compareAndSetLease(String scopeKey, JSONObject expected, JSONObject replacement) throws Exception;
+}
